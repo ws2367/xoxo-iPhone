3f2d41d75ed34c4eb0ceb057bd78dcd889de06b0
irukimo <wangirukimo@gmail.com>
2013-09-27 14:06:50 -0700

    Reorganize

--- HomePageExample/Demo/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-
-#import "MHTabBarController.h"
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, MHTabBarControllerDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end
