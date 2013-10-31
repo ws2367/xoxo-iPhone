e81fc46ecd22074fe62d0bd57f64ded5882025cf
Shawn Shaw <wenhsiang.shaw@gmail.com>
2013-10-31 13:03:18 -0400

    Add XOXOUIViewController interface

---
+++ XOXOUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  XOXOUIViewController.h
+//  Cells
+//
+//  Created by Wen-Hsiang Shaw on 10/31/13.
+//  Copyright (c) 2013 WYY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XOXOUIViewController : UIViewController
+
+@end
