35181c58950182b444e2353b3c21e603b2e8b40f
Shawn Shaw <wenhsiang.shaw@gmail.com>
2014-03-25 23:30:29 -0700

    Added S3RequestResponder to handle asynchronous calls to S3 resources

---
+++ Clients/S3RequestResponder.h
@@ -0,0 +1,13 @@
+//
+//  S3Delegate.h
+//  Cells
+//
+//  Created by Wen-Hsiang Shaw on 3/25/14.
+//  Copyright (c) 2014 WYY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface S3Delegate : NSObject
+
+@end
---
+++ Clients/S3RequestResponder.m
@@ -0,0 +1,13 @@
+//
+//  S3Delegate.m
+//  Cells
+//
+//  Created by Wen-Hsiang Shaw on 3/25/14.
+//  Copyright (c) 2014 WYY. All rights reserved.
+//
+
+#import "S3Delegate.h"
+
+@implementation S3Delegate
+
+@end
